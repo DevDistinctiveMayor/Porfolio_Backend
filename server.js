edc36f7534627e7c22585195a1e286fe3c6eef46
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2024-08-08 13:57:01 +0100

    email_sent

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+
+const port = process.env.port || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
